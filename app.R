22d5289ef7f76baa8f6280d017f9d82842e932c8
Shannon <shan.crouch@gmail.com>
2018-11-11 13:04:45 -0500

    Profile pic added

--- app.R
+++ app.R
@@ -8,43 +8,22 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(shinydashboardPlus)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+ui <- dashboardPage(
+  dashboardHeader(title = "Shiny Portfolio"),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    widgetUserBox(
+      title="Shannon Crouch",
+      subtitle = "Data Insights Analyst",
+      type=2, #2
+      src="C:/Users/shann/OneDrive/Documents/Portfolio"
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+server <- function(input, output) { }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
-
+shinyApp(ui, server)
